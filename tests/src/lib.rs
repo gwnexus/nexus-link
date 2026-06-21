d45cc4588f8e900af3bcc25baf0ef002668bab74
Patrick Paechnatz <patrick.paechnatz@gmail.com>
2026-06-21 05:18:26 +0200

    fix: gate test modules with #[cfg(test)] to avoid unused-import clippy errors

--- tests/src/lib.rs
+++ tests/src/lib.rs
@@ -1,6 +1,12 @@
+#[cfg(test)]
 mod config_tests;
+#[cfg(test)]
 mod error_tests;
+#[cfg(test)]
 mod preflight_tests;
+#[cfg(test)]
 mod telemetry_tests;
+#[cfg(test)]
 mod token_tests;
+#[cfg(test)]
 mod types_tests;
